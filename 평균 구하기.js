02ce63852cd6d420757657ba454945346a501679
ChuSeongJun <91831079+ChuSeongJun@users.noreply.github.com>
2023-05-30 22:44:28 +0900

    추가

---
+++ 평균 구하기.js
@@ -0,0 +1,16 @@
+function solution(arr) {
+    var answer = 0;
+    let sum=0;
+    for(let i =0; i<arr.length; i++){
+        sum+=arr[i]
+    }
+    answer=sum/arr.length;
+
+
+    return answer;
+}
+
+
+// function average(array){
+//     return array.reduce((a,b) => a+b) / array.length;
+// }
