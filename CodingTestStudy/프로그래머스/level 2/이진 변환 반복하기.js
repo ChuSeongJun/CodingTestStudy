dce5aaa543b40b37061e7e848744beb77a59ddb7
ChuSeongJun <91831079+ChuSeongJun@users.noreply.github.com>
2023-11-24 18:30:30 +0900

    완료

--- CodingTestStudy/프로그래머스/level 2/이진 변환 반복하기.js
+++ CodingTestStudy/프로그래머스/level 2/이진 변환 반복하기.js
@@ -1,4 +1,14 @@
 function solution(s) {
-    var answer = [];
-    return answer;
+    let result = [0, 0];
+    let len = 0;
+
+    while(s.length>1){
+        len=s.length;
+        s=s.split("0").join("");
+        result[0]++;
+        result[1]+=len-s.length;
+        s=s.length.toString(2);
+    }
+
+    return result;
 }
