c3d6f200bd4e8effe386f9ee62c71177445a467c
ChuSeongJun <91831079+ChuSeongJun@users.noreply.github.com>
2023-12-01 21:11:03 +0900

    피보나치로 해결

---
+++ CodingTestStudy/프로그래머스/level 2/멀리뛰기.js
@@ -0,0 +1,12 @@
+function solution(n) {
+    var answer=0;
+    let dp=[];
+    dp[0]=1;
+    dp[1]=1;
+
+    for(let i=2; i<=n; i++){
+        dp[i]=dp[i-1]+dp[i-2]%1234567;
+    }
+    answer=dp[n];
+    return answer%1234567;
+}
