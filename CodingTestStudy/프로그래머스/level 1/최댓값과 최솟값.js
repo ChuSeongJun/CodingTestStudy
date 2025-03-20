887fe3464f631c036d0e2631f74fd56564429845
ChuSeongJun <j949854@gmail.com>
2025-03-20 20:35:23 +0900

    중복 파일 제거

--- CodingTestStudy/프로그래머스/level 1/최댓값과 최솟값.js
+++
@@ -1,9 +0,0 @@
-function solution(s) {
-    const numbers = s.split(' ').map(Number);
-    const max = Math.max(...numbers);
-    const min = Math.min(...numbers);
-    return `${min} ${max}`;
-}
-
-
-console.log(solution("1 2 3 4"));
