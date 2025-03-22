ed3fe0ff9d78e97c9166e70421f452dc6e8b4a59
ChuSeongJun <j949854@gmail.com>
2025-03-23 00:40:27 +0900

    재귀로 풀기기

---
+++ 프로그래머스/0/181919. 콜라츠 수열 만들기/practice.js
@@ -0,0 +1,8 @@
+function solution(n, arr = []) {
+    arr.push(n);
+    if (n === 1) return arr;
+    if (n % 2 === 0) return solution(n / 2, arr);
+    return solution(3 * n + 1, arr);
+}
+
+// 재귀로 푸는법
