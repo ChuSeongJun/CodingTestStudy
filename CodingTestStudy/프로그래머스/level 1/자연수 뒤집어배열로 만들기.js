99ec19d104a5210ebaa4db5b392e28b8baa31134
ChuSeongJun <91831079+ChuSeongJun@users.noreply.github.com>
2025-08-03 18:22:13 +0900

    Feat: 동기화

--- CodingTestStudy/프로그래머스/level 1/자연수 뒤집어배열로 만들기.js
+++
@@ -1,9 +0,0 @@
-function solution(n) {
-    var answer = [];
-
-    console.log(n);
-    answer=String(n).split('').reverse().map(num=>Number(num));
-    return answer;
-}
-
-console.log(solution(12345))
