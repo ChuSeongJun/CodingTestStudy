348d1cef333f4fe51785f383be38f50dd7ecdb0d
추성준 <91831079+ChuSeongJun@users.noreply.github.com>
2025-08-11 01:32:14 +0900

    [level 0] Title: 문자열이 몇 번 등장하는지 세기, Time: 0.21 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181871. 문자열이 몇 번 등장하는지 세기/문자열이 몇 번 등장하는지 세기.js
@@ -0,0 +1,7 @@
+function solution(myString, pat) {
+    var answer = 0;
+    for (let i = 0; i < myString.length; i++) {
+        if (myString.slice(i, i + pat.length).includes(pat)) answer += 1;
+    }
+    return answer;
+}
