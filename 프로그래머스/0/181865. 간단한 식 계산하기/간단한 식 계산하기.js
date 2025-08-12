2239db211007419582a8bb73ea4c05cf8ad556a6
ChuSeongJun <j949854@gmail.com>
2025-08-12 22:26:11 +0900

    다른 사람 풀이  추가

--- 프로그래머스/0/181865. 간단한 식 계산하기/간단한 식 계산하기.js
+++ 프로그래머스/0/181865. 간단한 식 계산하기/간단한 식 계산하기.js
@@ -1,12 +1,24 @@
 function solution(binomial) {
     var answer = 0;
     const [a, op, b] = binomial.split(" ");
-    if(op==="+"){
+    if (op === "+") {
         answer = Number(a) + Number(b);
-    }else if(op==="-"){
+    } else if (op === "-") {
         answer = Number(a) - Number(b);
-    }else if(op==="*"){
+    } else if (op === "*") {
         answer = Number(a) * Number(b);
     }
     return answer;
 }
+
+// 다른 사람 풀이
+function solution2(binomial) {
+    const [a, op, b] = binomial.split(" ");
+    return ops[op](Number(a), Number(b));
+}
+
+const ops = {
+    "+": (a, b) => a + b,
+    "-": (a, b) => a - b,
+    "*": (a, b) => a * b,
+};
