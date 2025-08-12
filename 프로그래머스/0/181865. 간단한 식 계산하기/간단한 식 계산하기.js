50750e1f43705ec4f8080bf1eb421ffb46772665
추성준 <91831079+ChuSeongJun@users.noreply.github.com>
2025-08-12 22:24:24 +0900

    [level 0] Title: 간단한 식 계산하기, Time: 0.11 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181865. 간단한 식 계산하기/간단한 식 계산하기.js
@@ -0,0 +1,12 @@
+function solution(binomial) {
+    var answer = 0;
+    const [a, op, b] = binomial.split(" ");
+    if(op==="+"){
+        answer = Number(a) + Number(b);
+    }else if(op==="-"){
+        answer = Number(a) - Number(b);
+    }else if(op==="*"){
+        answer = Number(a) * Number(b);
+    }
+    return answer;
+}
